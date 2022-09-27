6ee9d05d30d567ad9f5b6cd08c491cd1e5d0371b
ivy <li.ivy5@gmail.com>
2022-09-27 12:13:21 +1000

    keyboard working

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ import {generateWord, isGuessValid} from './wordle-words.js';
 const targetWord = generateWord();
 console.log(targetWord);
 let message = document.getElementById('message');
+let guessWord = document.getElementById('guessWord');
+
+// guessWord.value.toUpperCase();
+let keys = document.getElementsByClassName('key');
+for (let keyElement of keys) {
+    if (keyElement.textContent != 'Enter' && keyElement.textContent != 'Del') {
+        keyElement.textContent = keyElement.textContent.toUpperCase();
+        keyElement.setAttribute('id', keyElement.textContent);
+    }
+}
 // const targetWord = 'PEACH';
 
 const guesses = [
@@ -33,6 +43,13 @@ function renderBoard(currentGuesses) {
 
             if (letter === targetObj[colNum]) {
                 columnsDiv.children[colNum].style.background = 'green'
+                // for (let keyElement of keys) {
+                //     if (keyElement.textContent.toLocaleUpperCase() === targetObj[colNum]) {                     
+                //         keyElement.style.background = 'green'
+                //     }
+                // }
+                let keyId = targetObj[colNum]
+                document.getElementById(keyId).style.background = 'green';
                 delete targetObj[colNum]
                 delete guessObj[colNum]
             }
@@ -43,12 +60,33 @@ function renderBoard(currentGuesses) {
 
             if (letter !== targetObj[colNum] && Object.values(targetObj).includes(letter)) {
                 columnsDiv.children[colNum].style.background = 'yellow'
+                // for (let keyElement of keys) {
+                //     if (keyElement.textContent.toLocaleUpperCase() === letter && !keyElement.style.background) {                     
+                //         keyElement.style.background = 'yellow'
+                //     }
+                // }
+                let keyId = letter;
+                const yellowKey = document.getElementById(keyId);
+                if (!yellowKey.style.background) {
+                    yellowKey.style.background = 'yellow'
+                }
                 const col = getKeyByValue(targetObj, letter);
                 delete targetObj[col]
                 delete guessObj[colNum]
             }
             else {
                 columnsDiv.children[colNum].style.background = 'grey';
+                // for (let keyElement of keys) {
+                //     if (keyElement.textContent.toLocaleUpperCase() === columnsDiv.children[colNum].textContent 
+                //         && !keyElement.style.background) {                     
+                //         keyElement.style.background = 'grey'
+                //     }
+                // }
+                let keyId = columnsDiv.children[colNum].textContent
+                const greyKey = document.getElementById(keyId);
+                if (!greyKey.style.background) {
+                    greyKey.style.background = 'grey'
+                }
             }
         })
     
@@ -67,7 +105,7 @@ function decideGameState() {
         message.innerText = "Congratulations! You won."
     }
     else if (!guesses.includes('')) {
-        message.innerText = 'You have exhausted all your guesses!'
+        message.innerText = `You have exhausted all your guesses! The word you are looking for is ${targetWord}`
         // document.getElementById('guessWord').value = ''
     }
     
@@ -79,20 +117,51 @@ function decideGameState() {
 
 document.getElementById('submitGuess').addEventListener('click', handleClick);
 function handleClick() {
-   
+    // console.log(guessWord.textContent);
     message.innerText = ''
-    const yourGuess = document.getElementById('guessWord').value.toUpperCase();
+    const yourGuess = guessWord.value.toUpperCase();
     if (guesses.includes('') && isGuessValid(yourGuess)) {
         const idx = guesses.indexOf('');
         guesses[idx] = yourGuess;
         renderBoard(guesses)
-        document.getElementById('guessWord').value = '';
+        guessWord.value = '';
     }
     else if (!isGuessValid(yourGuess)) {
         message.innerText = 'Please enter a valid word!'
-        document.getElementById('guessWord').value = ''
+        guessWord.value = ''
     }
     decideGameState()
 }
     
+// Keyboard
 
+
+function generateGuessWord() {
+    let keyboardGuess = ''
+    for (let keyElement of keys) {
+        let key = keyElement.textContent;
+        keyElement.addEventListener('click', function () {
+            switch(key) {
+                case "Del":
+                    guessWord.value = guessWord.value.slice(0, guessWord.value.length-1);
+                    break;
+                case "Enter":
+                    handleClick();
+                    keyboardGuess = '';
+                    break;
+                default: 
+                    keyboardGuess = keyboardGuess.concat(key);
+                    guessWord.value = keyboardGuess;
+
+            }
+        })
+    }
+}
+
+// function renderKeyboard() {
+//     for (let keyElement of keys) {
+//         for ()
+//     }
+// }
+
+generateGuessWord()
